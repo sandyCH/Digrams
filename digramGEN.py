b5b7b085f363a2d5e7b88c17ad41618064fe8c03
murphy <mac3iii@gmail.com>
2019-01-25 16:03:49 -0500

    Add files via upload
    Generates a python dictionary of digrams called digrams.py

---
+++ digramGEN.py
@@ -0,0 +1,34 @@
+#python3, Ubuntu linux
+#Natural Language Tool Kit in: http://www.nltk.org/install.html
+#NLTK Data explained in: http://www.nltk.org/data.html
+#sudo pip3 install -U nltk
+#python3
+# >>> import nltk
+# >>> nltk.download()
+#Go ahead and download all, then run the script below:
+print('Working....this may take a minute or two....')
+
+import nltk
+from nltk.util import ngrams
+
+cpus = ''
+for a in nltk.corpus.reuters.fileids():
+    cpus += nltk.corpus.reuters.raw(a)
+
+chrs = [c for c in cpus]
+
+bgs = ngrams(chrs,2)
+fdist = nltk.FreqDist(bgs)
+
+print('\nReuters 20 commonest digrams:',[(''.join(k),v) for k,v in fdist.most_common(20)])
+
+fdic = {''.join(k):v for k,v in fdist.items()}
+
+tot1 = sum([a for b,a in fdic.items()])
+print('\nTotal digrams:',tot1)
+tot2 = len([b for b,a in fdic.items()])
+print('Total entries:',tot2,'\n')
+
+outfile = open('digrams.py','w')
+outfile.write('fdc = ' + str(fdic))
+outfile.close()
